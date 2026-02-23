5579ff3dc460d3ddce16d38be1ab77c4668ce308
Maksim Glamozdenko <m.bartomeo@mail.ru>
2026-02-23 20:52:40 +0300

    Исправляю ошибки кода

--- src/main.js
+++ src/main.js
@@ -47,13 +47,13 @@ function analyzeSalesData(data, options) {
     // @TODO: Проверка входных данных
 
     if (
-        !data 
-        || !Array.isArray(data.sellers)
-        || data.sellers.length === 0
-        || !Array.isArray(data.products)
-        || data.products.length === 0
-        || !Array.isArray(data.purchase_records)
-        || data.purchase_records.length === 0
+        !data ||
+        !Array.isArray(data.sellers) ||
+        data.sellers.length === 0 ||
+        !Array.isArray(data.products) ||
+        data.products.length === 0 ||
+        !Array.isArray(data.purchase_records) ||
+        data.purchase_records.length === 0 
        ) {
         throw new Error('Некорректные входные данные');
     }
